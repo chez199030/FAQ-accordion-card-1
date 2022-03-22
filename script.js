b53b6dea52287798256f6f59a84cfffa1e6f56fe
chez199030 <62951805+chez199030@users.noreply.github.com>
2022-03-22 18:19:56 +0800

    Finish

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// const question = document.querySelectorAll(".question");
+
+// question.forEach((question) => question.addEventListener("click", () => {}));
+
+this.addEventListener("DOMContentLoaded", () => {
+  const questions = document.querySelectorAll(".question");
+  questions.forEach((question) =>
+    question.addEventListener("click", () => {
+      if (question.parentNode.classList.contains("active")) {
+        question.parentNode.classList.toggle("active");
+      } else {
+        questions.forEach((question) =>
+          question.parentNode.classList.remove("active")
+        );
+        question.parentNode.classList.add("active");
+      }
+    })
+  );
+});
